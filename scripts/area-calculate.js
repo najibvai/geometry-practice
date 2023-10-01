075470ae46ccd859069cb4d7eabbff7f70306b84
najibvai <najibjadran5@gmail.com>
2023-10-02 01:30:40 +0600

    Ellipse added

--- scripts/area-calculate.js
+++ scripts/area-calculate.js
@@ -21,10 +21,11 @@ function calculateTriangleArea() {
     areaSpan.innerText = area;
 }
 
-// Rectangle Area
+
+// -------------------------------------Rectangle Area--------------------------------------
 function calculateRectangleArea() {
     // Rectangle Width Field
-    const widthField = document.getElementById('triangle-width');
+    const widthField = document.getElementById('rectangle-width');
     const widthValueText = widthField.value;
     const width = parseFloat(widthValueText);
     console.log(width);
@@ -32,7 +33,7 @@ function calculateRectangleArea() {
     widthField.value = '';
 
     // Rectangle Length Field
-    const lengthField = document.getElementById('triangle-length');
+    const lengthField = document.getElementById('rectangle-length');
     const lengthValueText = lengthField.value;
     const length = parseFloat(lengthValueText);
     console.log(length);
@@ -42,6 +43,42 @@ function calculateRectangleArea() {
     const area = width * length;
     console.log(area);
 
-    const areaSpan = document.getElementById('rectangle-area');
-    areaSpan.innerText = area;
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
+
+// --------------------------------------------------------------
+
+function calculateParallelogramArea(){
+    const base = getInputValue('parallelogram-base');
+    console.log(base);
+    const height = getInputValue('parallelogram-height');
+    console.log(height);
+    const area = base * height;
+    setElementInnerText('parallelogram-area', area)
+}
+// --------------------------------------------------------------
+
+function calculateEllipseArea(){
+    const majorRadius = getInputValue('ellipse-major-radius');
+    const minorRadius = getInputValue('ellipse-minor-radius');
+    const ellipseResult = 3.14 * majorRadius * minorRadius;
+    setElementInnerText('ellipse-area', ellipseResult)
+}
+// --------------------------------------------------------------
+
+// reusable function -----> reduce duplicate code
+
+function getInputValue(fieldId){
+    const inputField = document.getElementById(fieldId);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    inputField.value = '';
+    return value;
+}
+
+function setElementInnerText(elementId, result){
+    const element = document.getElementById(elementId);
+    element.innerText = result;
+}
+
